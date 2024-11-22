68a98bc50b69febf9fb045d705b501120cae078d
jonatasem <jonatas.em20@gmail.com>
2024-11-22 11:40:33 -0300

    melhorando responsividade

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,32 +14,35 @@ export default function Header({ onFilterChange }) {
         <header className='container-header'>
             <h1>Avaliações</h1>
             <nav className="nav-bar">
-                <select value={unidadFilter} onChange={(e) => setUnidadFilter(e.target.value)}>
-                    <option value="">Unidades</option>
-                    <option value="Unidade A">Unidade A</option>
-                    <option value="Unidade B">Unidade B</option>
-                    <option value="Unidade C">Unidade C</option>
-                </select>
-                <select value={professionalFilter} onChange={(e) => setProfessionalFilter(e.target.value)}>
-                    <option value="">Profissionais</option>
-                    <option value="Maria Silva">Maria Silva</option>
-                    <option value="João Pereira">João Pereira</option>
-                    <option value="Ana Costa">Ana Costa</option>
-                    <option value="Carlos Oliveira">Carlos Oliveira</option>
-                    <option value="Fernanda Santos">Fernanda Santos</option>
-                    <option value="Ricardo Almeida">Ricardo Almeida</option>
-                </select>
-                <select value={ratingFilter} onChange={(e) => setRatingFilter(e.target.value)}>
-                    <option value="">Notas</option>
-                    <option value="1">5.0</option>
-                    <option value="2">4.0</option>
-                    <option value="3">3.0</option>
-                    <option value="4">2.0</option>
-                    <option value="5">1.0</option>
-                    <option value="Não Avaliaram">Não Avaliaram</option> {/* Adicionando opção */}
-                </select>
-                <button onClick={handleFilterChange} aria-label="Aplicar filtros">Filtrar</button>
+                <section>
+                    <select value={unidadFilter} onChange={(e) => setUnidadFilter(e.target.value)}>
+                        <option value="">Unidades</option>
+                        <option value="Unidade A">Unidade A</option>
+                        <option value="Unidade B">Unidade B</option>
+                        <option value="Unidade C">Unidade C</option>
+                    </select>
+                    <select value={professionalFilter} onChange={(e) => setProfessionalFilter(e.target.value)}>
+                        <option value="">Profissionais</option>
+                        <option value="Maria Silva">Maria Silva</option>
+                        <option value="João Pereira">João Pereira</option>
+                        <option value="Ana Costa">Ana Costa</option>
+                        <option value="Carlos Oliveira">Carlos Oliveira</option>
+                        <option value="Fernanda Santos">Fernanda Santos</option>
+                        <option value="Ricardo Almeida">Ricardo Almeida</option>
+                    </select>
+                    <select value={ratingFilter} onChange={(e) => setRatingFilter(e.target.value)}>
+                        <option value="">Notas</option>
+                        <option value="1">5.0</option>
+                        <option value="2">4.0</option>
+                        <option value="3">3.0</option>
+                        <option value="4">2.0</option>
+                        <option value="5">1.0</option>
+                        <option value="Não Avaliaram">Não Avaliaram</option> {/* Adicionando opção */}
+                    </select>
+                </section>
+                <button className="btn-filter" onClick={handleFilterChange} aria-label="Aplicar filtros">Filtrar</button>
             </nav>
+            <button className="btn-mobile"></button>
         </header>
     );
 }
